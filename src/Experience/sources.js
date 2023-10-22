3b54000163828e0f74b90ecbd49409214eb1b745
Théotime Lameyre <theotimelameyre@MacBook-Pro-de-Theotime.local>
2023-10-22 19:41:06 +0200

    first commit

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -2,17 +2,17 @@ export default [
   {
     name: "landscape1",
     type: "texture",
-    path: "/images/360_images/1.jpg",
+    path: "/images/1.jpg",
   },
   {
     name: "landscape2",
     type: "texture",
-    path: "/images/360_images/2.jpg",
+    path: "/images/2.jpg",
   },
   {
     name: "landscape3",
     type: "texture",
-    path: "/images/360_images/3.jpg",
+    path: "/images/3.jpg",
   },
   {
     name: "pin",
